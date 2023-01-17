a71d91b3655cf6d61390a8e4018cd9c71b5359f9
Jiony Henrique <jiony@live.com>
2023-01-17 06:24:42 -0300

    Inicio do projeto

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import { Habit } from './components/Habit'
+
+function App() {
+	return (
+		<div>
+			<Habit completed={3} />
+			<Habit completed={2} />
+			<Habit completed={1} />
+			<Habit completed={5} />
+			<Habit completed={4} />
+		</div>
+	)
+}
+
+export default App
